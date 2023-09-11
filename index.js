d3af76350bff9354d731c78fa28d052efcd14abd
Sergio Ibagy <sibagy@amazon.com>
2023-09-11 16:49:43 -0700

    * adding names to agents * adding Conversation class, standUpComedians

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 exports.Agent = require('./lib/agent').Agent;
+exports.standUpComedians = require('./lib/conversations/standUpComedians');
 exports.comediansTalking = require('./lib/conversations/comediansTalking');
 exports.academicInterview = require('./lib/conversations/academicInterview');
 exports.podcast = require('./lib/conversations/podcast');
--- test.js
+++ test.js
@@ -1,11 +1,12 @@
-const { comediansTalking, podcast, academicInterview } = require('.');
+const { comediansTalking, podcast, academicInterview, standUpComedians } = require('.');
 
 const runner = async () => {
-    await podcast('Leonardo da Vinci', { conversationSize: 4 });
+    // await podcast('Leonardo da Vinci', { conversationSize: 4 });
     // await podcast('Albert Einstein', { conversationId: 'test-128', conversationSize: 2 });
     // await podcast('Albert Einstein', { conversationId: 'test-128', conversationSize: 1 });
     // await comediansTalking({ conversationSize: 5 });
-    // await academicInterview('Quantum Mechanics', { conversationSize: 10 });
+    await standUpComedians({ conversationSize: 6 });
+    // await academicInterview('Quantum Mechanics', { conversationSize: 3 });
 };
 
 runner();
