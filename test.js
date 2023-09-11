65285e9322f05a57c39d4f6d51806c2e1a16bdff
Sergio Ibagy <sibagy@amazon.com>
2023-09-11 12:54:23 -0700

    some changes: * wrap lines on console * add messages to promtp to wrap up conversation close to end * display character names (instead of UUID)

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 const { comediansTalking, podcast, academicInterview } = require('.');
 
 const runner = async () => {
+    await podcast('Leonardo da Vinci', { conversationSize: 4 });
+    // await podcast('Albert Einstein', { conversationId: 'test-128', conversationSize: 2 });
     // await podcast('Albert Einstein', { conversationId: 'test-128', conversationSize: 1 });
     // await comediansTalking({ conversationSize: 5 });
-    await academicInterview('Quantum Mechanics', { conversationSize: 10 });
+    // await academicInterview('Quantum Mechanics', { conversationSize: 10 });
 };
 
 runner();
